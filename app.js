9f438a85827bf7de618f30a14889af4b06d40fe8
Lir Algazi <liralgazi@gmail.com>
2023-07-01 16:41:07 +0300

    mysql connection

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dbService = require('./dbService');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+//create
+app.post('/insert', (request,response)=> {
+
+});
+
+//read 
+app.get('/getAll', (request,response)=> {
+    console.log('test');
+    response.json({
+        succsess: true
+    });
+
+});
+
+//update
+
+//delete
+
+app.listen(process.env.PORT, ()=> console.log('app is running'));
