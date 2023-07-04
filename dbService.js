3a11a8aa487196f336b5636b5101377f1af874ba
Lir Algazi <liralgazi@gmail.com>
2023-07-04 17:01:35 +0300

    finish integration with DB
    INSERT/ UPDATE/ DELETE/ POST

--- dbService.js
+++ dbService.js
@@ -23,23 +23,6 @@ class DbService {
     static getDbServiceInstance() {
         return instance ? instance : new DbService();
     }
-    /*
-    async getAllData() {
-        try {
-            const response = await new Promise((resolve, reject) => {
-                const query = "SELECT * FROM names;";
-
-                connection.query(query, (err, results) => {
-                    if (err) reject(new Error(err.message));
-                    resolve(results);
-                })
-            });
-            // console.log(response);
-            return response;
-        } catch (error) {
-            console.log(error);
-        }
-        */
        async getAllData(){
         try{
             const response = await new Promise((resolve, reject) => {
@@ -69,17 +52,76 @@ class DbService {
                     resolve(result.insertId);
                 })
             });
-            /*
+            
             return {
                 id : insertId,
                 name : name,
                 dateAdded : dateAdded
             };
-            */
+            
            console.log(insertId);
            //return response;
         } catch (error) {
             console.log(error);
+        }
+    }
+
+
+    async searchByName(users) {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM names WHERE users = ?;";
+
+                connection.query(query, [users], (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+
+    async updateNameById(id, users) {
+        try {
+            id = parseInt(id, 10); 
+            const response = await new Promise((resolve, reject) => {
+                const query = "UPDATE names SET users = ? WHERE id = ?";
+    
+                connection.query(query, [users, id] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+            });
+    
+            return response === 1 ? true : false;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+
+    async deleteRowById(id) {
+        try {
+            id = parseInt(id, 10); 
+            const response = await new Promise((resolve, reject) => {
+                const query = "DELETE FROM names WHERE id = ?";
+    
+                connection.query(query, [id] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+            });
+    
+            return response === 1 ? true : false;
+        } catch (error) {
+            console.log(error);
+            return false;
         }
     }
     }
